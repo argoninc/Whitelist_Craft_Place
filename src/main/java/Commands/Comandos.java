f6214dc64e7c33181acdcdf7e7267d8303188f4a
Gabriel Santos <o.gabriel@aluno.ufabc.edu.br>
2020-05-09 20:31:39 -0300

    Criação da estrutura, arquivos e pre-sets.

---
+++ src/main/java/Commands/Comandos.java
@@ -0,0 +1,5 @@
+package Commands;
+
+public class Comandos {
+
+}
---
+++ src/main/java/com/github/argoninc/Listener/Principal.java
@@ -0,0 +1,15 @@
+package com.github.argoninc.Listener;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Principal extends JavaPlugin{
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
